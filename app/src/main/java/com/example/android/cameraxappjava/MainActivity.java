e6995ab1275c6188ba10e0484a5ffa0e7c2c078b
hamid <khadem.hamid@gmail.com>
2022-01-19 23:10:05 +0330

    app run and show preview of camera

--- app/src/main/java/com/example/android/cameraxappjava/MainActivity.java
+++ app/src/main/java/com/example/android/cameraxappjava/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.camera_layout);
 //        setContentView(R.layout.camera_layout);
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
 
